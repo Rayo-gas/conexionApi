47f0aaa706d2b1927faad4ad4dc20746de3779df
Jesus Pulido <167794331+JesusDavidVargasPulido@users.noreply.github.com>
2024-08-01 09:20:55 -0500

    Add files via upload

---
+++ whatsaap/coneccion_v1.py
@@ -0,0 +1,45 @@
+#librerias 
+import pyodbc 
+import pandas as pd
+import pyautogui, webbrowser
+from time import sleep 
+
+#region Variables
+query = 'SELECT Movil FROM xls.LineasRayogas WHERE  Valor IS NULL'
+celular = '3176655253'
+url = 'https://web.whatsapp.com/send?phone=+57'
+Repetirms = 3
+mensaje = 'Prueba api whastapp rayogas area TI DATOS'
+
+
+#Se hace la conexión
+conexionString ='DRIVER={SQL Server};SERVER=192.168.0.139\BODEGA;DATABASE=DataStage;UID=odbc;PWD=Odbc.2019'
+try:
+    conexion = pyodbc.connect(conexionString)
+    print('Exito')
+except: 
+    print('Error')
+
+#abrir la conexión 
+cursor = conexion.cursor()
+
+# Llamado la base de Datos
+cursor.execute (query)
+for row in cursor:
+    #webbrowser.open(url+row)
+    #sleep(6)
+
+    ##Ciclo para enviar mensaje 
+    #for i in range(Repetirms):
+    #    pyautogui.typewrite(mensaje)
+    #    pyautogui.press('enter')
+    print(row)   
+    
+#abri el whatsaap   
+webbrowser.open(url+celular)
+#tiempo de espera para enviar mensaje el tienpo no puede ser menor de 6 segundos 
+sleep(6)
+#Ciclo para enviar mensaje 
+for i in range(Repetirms):
+    pyautogui.typewrite(mensaje)
+    pyautogui.press('enter')
